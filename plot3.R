fbc52feb6dd6e571cc46800ea55135898e16e813
murty bavisetty <bavisettys@gmail.com>
2015-09-13 17:57:07 -0700

    modified files

--- plot3.R
+++ plot3.R
@@ -28,16 +28,17 @@ x <- as.POSIXct(paste(data2$Date, data2$Time), format ="%Y-%m-%d %H:%M:%S")
 data2 <- cbind(DateTime = x, data2)
 
 ## Plot all the 4 sub plots under the same plot and save as plot3
-with (data2, {
-  plot(DateTime, Global_reactive_power, type="l", ylab = "Global_reactive_power", 
-                                                              xlab = "datetime", type ="n")
-  plot(DateTime, Sub_metering_1, type="l", ylab = "Energy Submetering", xlab = "") ## default color is black
-  plot(DateTime, Sub_metering_2, type="l", ylab = "Energy Submetering", col="red")
-  plot(DateTime, Sub_metering_3, type="l", ylab = "Energy Submetering", col="blue")
 
- legend("topright", col=c("black","red", "blue"), legend= c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
- legend("topright", pch = 1, col=c("black","red", "blue"), legend= c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
-} )
+  with (data2, {
+  plot(DateTime, Sub_metering_1, ylab = "Energy Sub metering", xlab = "", type ="n")
+  lines(DateTime, Sub_metering_1, col="black") 
+  lines(DateTime, Sub_metering_2, col="red" )
+  lines(DateTime, Sub_metering_3, col="blue")
+
+  legend("topright", lty = 1, col=c("black","red", "blue"), legend= c("Sub_metering_1",
+                                                                "Sub_metering_2", "Sub_metering_3"))
+  })
+  
 ## copy the image to as follows and save to plot3.png as follows
 
 dev.copy(png, file = "plot3.png", width = 480, height = 480)
